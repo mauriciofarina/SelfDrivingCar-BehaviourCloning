ec8760b64cb29bb4fc909203be87b7bb1970c873
Mauricio Farina <mauriciodfarina@gmail.com>
2019-06-18 12:09:51 -0300

    refactor: Save images on CarSample object as as arrays instead of numpy arrays

--- CarSample.py
+++ CarSample.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 class CarSample:
     
     def __init__(self, centerImg, leftImg, rightImg, steering, throttle, brake, speed, name):
-        self.centerImg = np.array(centerImg)
-        self.leftImg = np.array(leftImg)
-        self.rightImg = np.array(rightImg)
+        self.centerImg = centerImg
+        self.leftImg = leftImg
+        self.rightImg = rightImg
         self.steering = float(steering)
         self.throttle = float(throttle)
         self.brake = float(brake)
